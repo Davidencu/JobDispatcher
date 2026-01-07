770fa1d9cf76e37148c1085998ead4e736f4a7cb
Davidencu <davidencu2004@gmail.com>
2026-01-07 17:41:55 +0200

    Completed the function that generates anagrams

--- job_dispatcher.c
+++ job_dispatcher.c
@@ -3,7 +3,7 @@
 #include<stdint.h>
 #include<stdlib.h>
 #include<stdbool.h>
-#include<mpi.h>
+// #include<mpi.h>
 
 bool isPrime(int n)
 {
@@ -21,7 +21,7 @@ int count_primes(int n)
     int tmp=0;
     for(int i=2;i<n;i++)
     {
-        if (isPrime(n))
+        if (isPrime(i))
         {
             tmp++;
         }
@@ -41,10 +41,9 @@ int count_prime_divisors(int n)
     return count;
 }
 
-void anagrams(char *str)
+void generate_anagram_set(char *str, char *anagram_set)
 {
-    char arr[80] = {0}; //array of frequencies
-    char anagram[100] = {0}; //maximum of 100 characters
+    char arr[256] = {0}; //array of frequencies
     int anagram_length=0;
     for(int i=0;i<strlen(str);i++)
     {
@@ -54,14 +53,81 @@ void anagrams(char *str)
     {
         if(arr[i]!=0)
         {
-            anagram[anagram_length++]=i+'A';
+            anagram_set[anagram_length++]=i+'A';
         }
     }
-    //backtracking
-    
+    anagram_set[anagram_length]='\0';
+}
+
+int valid(char *x,int k)
+{
+    for(int i=0;i<k;i++)
+    {
+        if(x[i]==x[k])
+            return 0;
+    }
+    return 1;
+}
+
+int solution(int k,int n)
+{
+    if(k == n-1)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int factorial(int n)
+{
+    int tmp = 1;
+    for(int i = 2;i <= n;i++)
+    {
+        tmp *= i;
+    }
+    return tmp;
+}
+
+void generate_permutations(char *no_duplicates_set,char *permutations_set,char *x,int k,int n)
+{
+    for(int i = 0;i < n;i++)
+    {
+        x[k]=no_duplicates_set[i];
+        if(valid(x,k))
+        {
+            if(solution(k,n))
+            {
+                x[k+1]='\n';
+                x[k+2]='\0';
+                strcat(permutations_set,x);
+            }
+            else
+            {
+                generate_permutations(no_duplicates_set, permutations_set, x, k+1, n);
+            }
+        }
+    }
+}
+
+char *generate_anagrams(char *str)
+{
+    char no_duplicates_set[10] = {0};
+    generate_anagram_set(str,no_duplicates_set);
+    //printf("%s\n",no_duplicates_set);
+    int n = strlen(no_duplicates_set);
+    char *permutations_set=(char *)malloc((factorial(n) * (n+1) * sizeof(char)) + 1);
+    char x[10] = {0};
+    permutations_set[0]='\0';
+    generate_permutations(no_duplicates_set,permutations_set,x,0,n);
+    return permutations_set;
 }
 
 int main(int argc, char **argv)
 {
+    //char *permutations_set = generate_anagrams("sunshine");
+    //printf("%s\n",permutations_set);
+    //free(permutations_set);
+    int primes = count_primes(9);
+    printf("%d\n",primes);
     return 0;
 }
